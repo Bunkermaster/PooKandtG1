8f9c1c57cbcd9a636d1b964eb49cd1ddd4071745
bunkermaster <yann.le-scouarnec@hetic.net>
2018-06-06 16:27:25 +0200

    +add +delete +index actions

---
+++ src/Helper/AbstractController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Helper;
+
+/**
+ * Class AbstractController
+ * @author Yann Le Scouarnec <bunkermaster@gmail.com>
+ * @package Helper
+ */
+class AbstractController
+{
+    /**
+     * @var AbstractModel
+     */
+    protected $model;
+
+    /**
+     *
+     */
+    protected $view;
+}
